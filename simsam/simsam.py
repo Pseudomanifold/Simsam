062651adedc5f1ea15f5749da7a55ebdfa82eda6
Bastian Rieck <bastian@rieck.ru>
2021-01-03 21:54:04 +0100

    Implemented naive sampling algorithm

---
+++ simsam/simsam.py
@@ -0,0 +1,27 @@
+"""Algorithms for sampling from the unit simplex."""
+
+import numpy as np
+
+
+def naive_sampling(n, N=1):
+    """Sample from an n-dimensional simplex using a naive algorithm."""
+    P = []
+
+    for _ in range(N):
+        a = np.asarray([np.random.uniform() for _ in range(n - 1)])
+        p = [a[i] * np.prod(1 - a[:i-1]) for i in range(n - 1)]
+        p.append(1 - np.sum(p))
+
+        # Append sample to list of all samples that are to be returned
+        # by this function.
+        P.append(p)
+
+    return P
+
+
+# HIC SVNT DRACONES
+
+P = naive_sampling(2, 20000)
+
+for x, y in P:
+    print(x, y)
