d394b6111dc1a534ebba373c1aeb9a4e3cbd9635
Bastian Rieck <bastian@rieck.ru>
2021-01-03 22:26:06 +0100

    Implemented Kraemer's method
    Did not yet implement the fixed version of this algorithm, but it will
    do for now.

--- simsam/simsam.py
+++ simsam/simsam.py
@@ -1,6 +1,7 @@
 """Algorithms for sampling from the unit simplex."""
 
 import numpy as np
+import sys
 
 
 def naive_sampling(n, N=1):
@@ -19,9 +20,30 @@ def naive_sampling(n, N=1):
     return P
 
 
+def kraemer_sampling(n, N=1):
+    """Sample from an n-dimensional simplex using Kraemer's algorithm."""
+    M = sys.maxsize
+    P = []
+
+    for _ in range(N):
+
+        rng = np.random.default_rng()
+
+        X = rng.choice(M - 1, replace=False, size=n - 1)
+        X = sorted(X)
+        X = [0] + X + [M]
+        Y = np.diff(X)
+
+        # Store one of the generated samples, which is now guaranteed to
+        # lie on the unit simplex by construction.
+        P.append(np.asarray(Y) / M)
+
+    return P
+
+
 # HIC SVNT DRACONES
 
-P = naive_sampling(3, 20000)
+P = kraemer_sampling(3, 20000)
 
 for x, y, _ in P:
     print(x, y)
