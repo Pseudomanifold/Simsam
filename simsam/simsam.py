Fixed off-by-one error in sampling

simsam/simsam.py
@@ -9,7 +9,7 @@ def naive_sampling(n, N=1):
 
     for _ in range(N):
         a = np.asarray([np.random.uniform() for _ in range(n - 1)])
-        p = [a[i] * np.prod(1 - a[:i-1]) for i in range(n - 1)]
+        p = [a[i] * np.prod(1 - a[:i]) for i in range(n - 1)]
         p.append(1 - np.sum(p))
 
         # Append sample to list of all samples that are to be returned
@@ -21,7 +21,7 @@ def naive_sampling(n, N=1):
 
 # HIC SVNT DRACONES
 
-P = naive_sampling(2, 20000)
+P = naive_sampling(3, 20000)
 
-for x, y in P:
+for x, y, _ in P:
     print(x, y)