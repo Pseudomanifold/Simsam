b9a4176c5906f417a6979590741deeb25e24dc76
Bastian Rieck <bastian@rieck.ru>
2021-05-08 18:43:58 +0200

    Added tests and proper `__init__.py`

--- simsam/__init__.py
+++ simsam/__init__.py
@@ -1,1 +1,4 @@
 __version__ = '0.1.0'
+
+from .simsam import kraemer_sampling
+from .simsam import naive_sampling
--- tests/test_simsam.py
+++ tests/test_simsam.py
@@ -1,5 +1,17 @@
-from simsam import __version__
+import numpy as np
+
+from simsam import naive_sampling
+from simsam import kraemer_sampling
+
+from unittest import TestCase
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+class SmokeTest(TestCase):
+    """Simple smoke test to ensure that samples are valid."""
+
+    def test(self):
+        for f in [naive_sampling, kraemer_sampling]:
+            P = f(10, 1000)
+
+            for p in P:
+                self.assertAlmostEqual(np.sum(p), 1.0)
