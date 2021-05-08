8410d4cc07d55e17ff8d3ac60de811f13ac2fe08
Bastian Rieck <bastian@rieck.ru>
2021-05-08 18:12:30 +0200

    Updated algorithms

--- simsam/simsam.py
+++ simsam/simsam.py
@@ -5,7 +5,24 @@ import sys
 
 
 def naive_sampling(n, N=1):
-    """Sample from an n-dimensional simplex using a naive algorithm."""
+    """Sample from an n-dimensional simplex using a naive algorithm.
+
+    This is a naive sampling procedure that will not result in
+    samples being drawn uniformly from the n-simplex. However,
+    it is fast and might be suitable as a baseline algorithm.
+
+    Parameters
+    ----------
+    n : int
+        Dimension of simplex to sample from.
+    N : int
+        Number of points to sample.
+
+    Returns
+    -------
+    `np.array` of shape (N, n), with the rows corresponding to
+    individual samples.
+    """
     P = []
 
     for _ in range(N):
@@ -17,11 +34,36 @@ def naive_sampling(n, N=1):
         # by this function.
         P.append(p)
 
-    return P
+    return np.asarray(P, dtype=float)
 
 
-def kraemer_sampling(n, N=1):
-    """Sample from an n-dimensional simplex using Kraemer's algorithm."""
+def kraemer_sampling(n, N=1, full_support=True):
+    """Sample from an n-dimensional simplex using Kraemer's algorithm.
+
+    This is the preferred algorithm for sampling uniformly from the unit
+    n-simplex. Following Smith & Tromble _[1] the sampling procedure was
+    updated to ensure that the sampling is uniform across distributions,
+    provided they have full support. This behaviour can be changed using
+    the `full_support` parameter.
+
+    Parameters
+    ----------
+    n : int
+        Dimension of simplex to sample from.
+    N : int
+        Number of points to sample.
+    full_support : bool
+        If set, only provides distributions with full support, i.e. with
+        non-zero values only.
+
+    Returns
+    -------
+    `np.array` of shape (N, n), with the rows corresponding to
+    individual samples.
+
+    .. [1] Noah A. Smith and Roy W. Tromble, "Sampling Uniformly from
+    the Unit Simplex," 2004.
+    """
     M = sys.maxsize
     P = []
 
@@ -38,12 +80,12 @@ def kraemer_sampling(n, N=1):
         # lie on the unit simplex by construction.
         P.append(np.asarray(Y) / M)
 
-    return P
+    return np.asarray(P)
 
 
 # HIC SVNT DRACONES
 
-P = kraemer_sampling(3, 20000)
+P = naive_sampling(3, 20000)
 
 for x, y, _ in P:
     print(x, y)
