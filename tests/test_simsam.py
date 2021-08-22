270858a91c8eaa7df42f4d7e657436f5bbb05e06
Ethan Rooke <ethan@roo.ke>
2021-08-22 10:31:00 -0500

    Added a test case for the case where full support is false

--- tests/test_simsam.py
+++ tests/test_simsam.py
@@ -4,13 +4,14 @@ from simsam import naive_sampling
 from simsam import kraemer_sampling
 
 from unittest import TestCase
+from functools import partial
 
 
 class SmokeTest(TestCase):
     """Simple smoke test to ensure that samples are valid."""
 
     def test(self):
-        for f in [naive_sampling, kraemer_sampling]:
+        for f in [naive_sampling, kraemer_sampling, partial(kraemer_sampling, full_support = False)]:
             P = f(10, 1000)
 
             for p in P:
